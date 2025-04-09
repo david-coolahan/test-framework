d3c8397199a6e132f7b6c2cb982ac99ce1f6aa4d
David C <cooladave@gmail.com>
2025-04-10 02:17:53 +1000

    Update Invoke-Pester argument to fail Azure Pipeline if Pester tests fail

--- Accuro/Accuro.ps1
+++ Accuro/Accuro.ps1
@@ -12,9 +12,9 @@ foreach ( $module in $modules )
     }
 }
 Set-Location ./repo/src
-$result = Invoke-Pester -EnableExit
+$result = Invoke-Pester -PassThru
 
-# if ($result.FailedCount -gt 0) {
-#     Write-Host "$($result.FailedCount) tests failed."
-#     exit 1
-# }
+if ($result.FailedCount -gt 0) {
+    Write-Host "$($result.FailedCount) tests failed."
+    exit 1
+}
