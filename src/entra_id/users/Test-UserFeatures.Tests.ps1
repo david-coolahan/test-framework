8f7dbb9b546a73a409d425626a5c6341013cf115
David C <cooladave@gmail.com>
2025-04-21 23:17:50 +1000

    Create 'Entra ID - User Features' Pester file

---
+++ src/entra_id/users/Test-UserFeatures.Tests.ps1
@@ -0,0 +1,15 @@
+# BeforeAll {
+
+# }
+
+# Describe "User Features" {
+#     Context "User Features" {
+#         It "User can use preview features for My Apps" {
+            # No API Available
+#         }
+
+#         It "Administrators can access My Staff" {
+            # No API Available
+#         }
+#     }
+# }
