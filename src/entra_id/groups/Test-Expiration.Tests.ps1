5fdd5e40f09b6f30300d71f6148c985b0b5f8699
David C <cooladave@gmail.com>
2025-04-22 15:13:16 +1000

    Comment out Expiration pester file for demonstration This is due to expiration policy requiring a Entra ID P1 or P2 license

--- src/entra_id/groups/Test-Expiration.Tests.ps1
+++ src/entra_id/groups/Test-Expiration.Tests.ps1
@@ -1,27 +1,27 @@
 # Expiration policy for a M365 group equires Microsoft Entra ID P1 or P2 licenses (https://learn.microsoft.com/en-us/entra/identity/users/groups-lifecycle)
 
-BeforeAll {
-    Import-Module Pester
-    Connect-MgGraph -Scopes "Directory.Read.All"
-}
+# BeforeAll {
+#     Import-Module Pester
+#     Connect-MgGraph -Scopes "Directory.Read.All"
+# }
 
-Describe "Expiration" {
-    Context "Self Service Group Management" {
-        BeforeAll {
-            $ListGroupLifeCyclePolicy = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/groupLifecyclePolicies" -Method GET
-        }
+# Describe "Expiration" {
+#     Context "Self Service Group Management" {
+#         BeforeAll {
+#             $ListGroupLifeCyclePolicy = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/groupLifecyclePolicies" -Method GET
+#         }
 
-        It "Should have a group lifetime of 180 days" {
-            $ListGroupLifeCyclePolicy.GroupLifetimeInDays | Should -Be 180
-        }
+#         It "Should have a group lifetime of 180 days" {
+#             $ListGroupLifeCyclePolicy.GroupLifetimeInDays | Should -Be 180
+#         }
 
-        It "Should have an email contact for groups with no owners" {
-            $ListGroupLifeCyclePolicy.AlternateNotificationEmails | Should -Not -BeNullOrEmpty
-        }
+#         It "Should have an email contact for groups with no owners" {
+#             $ListGroupLifeCyclePolicy.AlternateNotificationEmails | Should -Not -BeNullOrEmpty
+#         }
 
-        It "Should enable expiration for these Microsoft 365 groups" {
-            $ListGroupLifeCyclePolicy.ManagedGroupTypes | Should -Be "All"
-        }
-    }
+#         It "Should enable expiration for these Microsoft 365 groups" {
+#             $ListGroupLifeCyclePolicy.ManagedGroupTypes | Should -Be "All"
+#         }
+#     }
 
-}
+# }
