d3400b800b4b09a9df5a25f65d056b76ba67f308
David C <cooladave@gmail.com>
2025-04-23 23:07:04 +1000

    Create 'Entra ID - Multifactor authentication registration policy' Pester file

---
+++ src/entra_id/protection/identity_protection/Test-MultifactorAuthenticationRegistrationPolicy.Tests.ps1
@@ -0,0 +1,8 @@
+# No API endpoint available
+# Requires P2 license
+
+# Describe "Multifactor Authentication Registration Policy" {
+#     Context "Multifactor Authentication Registration Policy" {
+#         No API Available
+#     }
+# }
