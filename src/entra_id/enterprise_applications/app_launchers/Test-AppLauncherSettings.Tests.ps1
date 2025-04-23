7b5d1cb7ab69e3936234bee215a5ec1c81592451
David C <cooladave@gmail.com>
2025-04-23 15:48:10 +1000

    Create 'Entra ID - App Launcher Settings' Pester file

---
+++ src/entra_id/enterprise_applications/app_launchers/Test-AppLauncherSettings.Tests.ps1
@@ -0,0 +1,9 @@
+# No public endpoint
+
+Describe "App Launcher Settings" {
+    # Context "Microsoft 365 Settings" {
+    #     It "Should allow users can only see Microsoft 365 apps in the Microsoft 365 portal" {
+             # No API Available
+    #     }
+    # }
+}
