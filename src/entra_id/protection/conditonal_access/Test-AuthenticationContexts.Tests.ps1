1996725438bd5decce1221b1322477a4571d5192
David C <cooladave@gmail.com>
2025-04-24 19:32:40 +1000

    Create 'Entra ID - Authentication Contexts' Pester file

---
+++ src/entra_id/protection/conditonal_access/Test-AuthenticationContexts.Tests.ps1
@@ -0,0 +1,23 @@
+BeforeAll {
+    Import-Module Pester
+    # Connect-MgGraph -Scopes "AuthenticationContext.Read.All"
+}
+
+Describe "Authentication Contexts" {
+    BeforeAll {
+        $GraphResult = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/identity/conditionalAccess/authenticationContextClassReferences" -Method GET
+        $AuthContext = $GraphResult.Value | Where-Object { $_.DisplayName -eq "PROTECTED information"}
+    }
+
+    It "Should have an authentication context called 'PROTECTED information'" {
+        $AuthContext | Should -Not -BeNullOrEmpty
+    }
+
+    It "Should be published to be used by apps" {
+        $AuthContext.IsAvailable | Should -Be $true
+    }
+
+    It "Should have the ID 'c1" {
+        $AuthContext.Id | Should -Be "c1"
+    }
+}
