0d77889829bedef226218905430e939770f87f87
David C <cooladave@gmail.com>
2025-04-24 21:36:31 +1000

    Create 'Entra ID - Authentication Strengths' Pester file

---
+++ src/entra_id/protection/conditonal_access/Test-AuthenticationStrengths.Tests.ps1
@@ -0,0 +1,25 @@
+BeforeAll {
+    Import-Module Pester
+    # Connect-MgGraph -Scopes "Policy.Read.All"
+}
+
+Describe "Authentication Strengths" {
+    Context "Phishing-resistant MFA and TAP" {
+        BeforeAll {
+            $GraphResponse = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/policies/authenticationStrengthPolicies" -Method GET
+            $Policy = $GraphResponse.Value | Where-Object { $_.DisplayName -eq "Phishing-resistant MFA and TAP" }
+            $AuthCombinationsList = $Policy.AllowedCombinations
+            $ExpectedAuthList = @(
+                "windowsHelloForBusiness"
+                "fido2"
+                "x509CertificateMultiFactor"
+                "temporaryAccessPassOneTime"
+                "temporaryAccessPassMultiUse"
+            )
+        }
+
+        It "Should have the correct authentication methods" {
+            $AuthCombinationsList | Sort-Object | Should -Be ($ExpectedAuthList | Sort-Object)
+        }
+    }
+}
