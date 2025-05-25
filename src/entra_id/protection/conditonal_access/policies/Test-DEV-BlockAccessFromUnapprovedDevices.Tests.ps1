00db3f583b8732cc29cb0e5a020dafebff9dccc9
David C <cooladave@gmail.com>
2025-05-25 16:52:09 +1000

    Create Users Context block for Dev-Block Access from unapproved devices Pester test file

--- src/entra_id/protection/conditonal_access/policies/Test-DEV-BlockAccessFromUnapprovedDevices.Tests.ps1
+++ src/entra_id/protection/conditonal_access/policies/Test-DEV-BlockAccessFromUnapprovedDevices.Tests.ps1
@@ -31,63 +31,30 @@ Describe "DEV - B - Block Access from Unapproved Devices" {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Context "Target Resources" {
         It "Should include all resources" {
-            $policy.TargetResources.Include | Should -Be "All resources"
         }
         It "Should not exclude any resources" {
-            $policy.TargetResources.Exclude | Should -BeEmpty
         }
     }
 
     Context "Device Platforms" {
         It "Should include Android, Windows Phone, macOS, and Linux" {
-            $policy.DevicePlatforms.Include | Should -Contain "Android"
-            $policy.DevicePlatforms.Include | Should -Contain "Windows Phone"
-            $policy.DevicePlatforms.Include | Should -Contain "macOS"
-            $policy.DevicePlatforms.Include | Should -Contain "Linux"
         }
         It "Should not include iOS or Windows" {
-            $policy.DevicePlatforms.Include | Should -NotContain "iOS"
-            $policy.DevicePlatforms.Include | Should -NotContain "Windows"
         }
         It "Should not exclude any platforms" {
-            $policy.DevicePlatforms.Exclude | Should -BeEmpty
         }
     }
 
     Context "Grant Controls" {
         It "Should block access" {
-            $policy.GrantControls | Should -Be "Block access"
         }
     }
 
     Context "Policy Enabled" {
         It "Should be enabled" {
-            $policy.Enabled | Should -Be $true
+
         }
     }
 }
